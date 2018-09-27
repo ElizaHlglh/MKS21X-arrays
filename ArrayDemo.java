04fda4b02c9eb2d702199949cb3df52a816ac1b6
ElizaHlglh <ewu4@stuy.edu>
2018-09-26 23:16:48 -0400

    ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,1 +1,19 @@
-
+public class ArrayDemo{
+  public static void main(String[] args) {
+    //Testing 0a.
+    int[] test0a = new int[] {1,2,3,4};
+    printArray(test0a);
+  }
+  //0a. Make a function to print a 1D array of ints.
+  public static void printArray(int[]ary){
+    System.out.print("testing 0a : [");
+    for (int i=0; i < ary.length; i++) {
+      if (i == ary.length-1) {
+        System.out.print(ary[i] + "] \n");
+      }
+      else {
+      System.out.print(ary[i] + ", ");
+      }
+    }
+  }
+}
